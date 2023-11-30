ceca146e366311665de5f5a2a24e253ec5a7dc7e
Levy <edgarlevy81@gmail.com>
2023-11-30 23:01:10 +0300

    Lab attempted

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,53 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakeries.append(bakery.to_dict())
+    if not bakeries:
+        response = make_response(jsonify({'message': 'No bakeries available'}), 404)
+    else:
+        response = make_response(jsonify(bakeries), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    response = make_response(
+        jsonify(bakery.to_dict()),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    goods = Bakery.query.all()
+    goods = []
+
+    for good in BakedGood.query.order_by(desc(BakedGood.price)).all():
+        goods.append(good.to_dict())
+
+    response = make_response(
+        jsonify(goods),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    goods_by_price = BakedGood.query.order_by(desc(BakedGood.price)).all()
+    if not goods_by_price:
+        response = make_response(jsonify({'message': 'No baked goods available'}), 404)
+    else:
+        goods = []
+        for good in goods_by_price:
+            goods.append(good.to_dict())
+        response = make_response(jsonify(goods[0]), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
